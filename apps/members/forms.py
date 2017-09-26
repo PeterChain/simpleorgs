c2e99b2cd9a98d165764129fd9f2a369a1a861c6
Correia <pedro.correia@cgi.com>
2017-09-26 18:27:09 +0100

    Start formset

--- apps/members/forms.py
+++ apps/members/forms.py
@@ -1,6 +1,8 @@
 from django.forms import ModelForm
 
 from .models import *
+from apps.address.models import address
+from apps.address.forms import AddressForm
 
 
 class MemberForm(ModelForm):
@@ -10,3 +12,7 @@ class MemberForm(ModelForm):
     class Meta:
         model = Member
         exclude = ('slug', 'address_book')
+
+MemberAddressFormset = inlineformset_factory(Member, Address,
+                                            form=MemberForm,
+                                            extra=1)
