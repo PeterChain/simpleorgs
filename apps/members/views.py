fadbe27e371d237595941066c7241bd8626dd618
Pedro Correia <pedroc25@msn.com>
2017-09-27 23:50:19 +0100

    Formview

--- apps/members/views.py
+++ apps/members/views.py
@@ -1,15 +1,36 @@
 from django.shortcuts import render
-from django.views.generic import DetailView, CreateView
+from django.forms import ChoiceField
+from django.views.generic import DetailView, FormView
 
-from .models import Member
+from .models import Member, MemberStatus
 from .forms import NewMemberForm
 
 
-class MemberCreate(CreateView):
+class MemberCreate(FormView):
+    """
+    View for creating a complex form of member/address
+    """
     form_class = NewMemberForm
     template_name = 'members/newmember.html'
+    success_url = '/'
 
-    
+    def get_initial(self):
+        """
+        Default values for the member
+        """
+        initial = super(MemberCreate, self).get_initial()
+
+        # Prepopulate member's status
+        """
+        statuses = MemberStatus.objects.all()
+        statuses_choices = []
+        for status in statuses:
+            statuses_choices.append(
+                (status.status_code, status.status_text)
+            )
+        initial['status'] = ChoiceField(choices=statuses_choices)
+        """
+        return initial
 
 
 class MemberDetail(DetailView):
